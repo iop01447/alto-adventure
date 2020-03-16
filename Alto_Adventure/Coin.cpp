99557efb0af7a6a394299d072f11971b400eab5e
조윤영 <iop01447@naver.com>
2020-03-16 16:35:31 +0900

    필터 업데이트

---
+++ Alto_Adventure/Coin.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h"
+#include "Coin.h"
+
+
+CCoin::CCoin()
+{
+}
+
+
+CCoin::~CCoin()
+{
+}
+
+void CCoin::Initialize()
+{
+}
+
+int CCoin::Update()
+{
+	return 0;
+}
+
+void CCoin::Late_Update()
+{
+}
+
+void CCoin::Render()
+{
+}
+
+void CCoin::Release()
+{
+}
---
+++ Alto_Adventure/Coin.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Obj.h"
+class CCoin :
+	public CObj
+{
+public:
+	CCoin();
+	virtual ~CCoin();
+
+	// CObj을(를) 통해 상속됨
+	virtual void Initialize() override;
+	virtual int Update() override;
+	virtual void Late_Update() override;
+	virtual void Render() override;
+	virtual void Release() override;
+};
+
